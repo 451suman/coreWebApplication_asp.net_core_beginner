53311db6ae8e63526e396426ee8b1243e9d7a161
451suman <sumanmushyakhwo@gmail.com>
2024-06-26 18:40:17 +0545

    Add project files.

---
+++ coreWebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using coreWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreWebApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<Customer> customers = new List<Customer>
+        {
+            new Customer() { Id = 1, Name = "John Doe", Address = "asdasdasdasd" },
+            new Customer (){ Id = 2, Name = "John Smith", Address = "asdkahsdnlk" }
+        };
+        public IActionResult Index()
+        {
+            ViewBag.message = "Customer Management System";
+            ViewBag.CustomerCount = customers.Count;
+            ViewBag.CustomerList = customers;
+            return View();
+        }
+        public IActionResult Details()
+        {
+            ViewData["message"] = "Customer Management System";
+            ViewData["CustomerCount"] = customers.Count;
+            ViewData ["CustomerList"] = customers;
+            return View();
+        }
+
+        public IActionResult tempdatademo()
+        {
+            TempData["message"] = "Customer Management System";
+            TempData["CustomerCount"] = customers.Count;
+            TempData["CustomerList"] = customers;
+            return View();
+        }
+        public IActionResult method2()
+        {
+            if (TempData["message"] == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["msg"] = TempData["message"].ToString();
+                return View();
+            }
+        }
+    }
+}
---
+++ coreWebApplication/Controllers/LoginLogout.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreWebApplication.Controllers
+{
+    public class LoginLogout : Controller
+    {
+        public IActionResult Login()
+        {
+            HttpContext.Session.SetString("Username", "suman");
+            return RedirectToAction("loginSuccess");
+        }
+        public IActionResult loginSuccess()
+        {
+            ViewBag.Uname= HttpContext.Session.GetString("Username");
+            return View();
+        }
+        public IActionResult Logouta()
+        {
+            try
+            {
+                if (HttpContext.Session.GetString("Username") != null)
+                {
+                    HttpContext.Session.Remove("Username");
+                }
+               // return View();
+               return RedirectToAction("Privacy", "Home");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return RedirectToAction("Error"); // Handle the exception, e.g., redirect to an error page
+            }
+        }
+
+    }
+}
